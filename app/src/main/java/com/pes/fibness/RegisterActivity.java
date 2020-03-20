6b99ed8abeaef6b37586a81fa3eaa56cc0292932
tanvir <tanvho@instarradell.cat>
2020-03-20 23:36:16 +0100

    Conexion BD, funciona el /test pero falla en post (/users)

--- app/src/main/java/com/pes/fibness/RegisterActivity.java
+++ app/src/main/java/com/pes/fibness/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.pes.fibness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.HasDefaultViewModelProviderFactory;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -35,6 +51,8 @@ public class RegisterActivity extends AppCompatActivity {
         register = (Button) findViewById(R.id.btn_register);
         backLogin = (TextView) findViewById(R.id.back_login);
 
+
+
         backLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,6 +61,8 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+
+        //PASSWORD
         password.addTextChangedListener(new TextWatcher()  {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -90,8 +110,97 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+
+
+        //REGISTER TO USER
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //insertUser();
+            }
+        });
+
     }
 
+    /*
+    private void insertUser() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        final String urlPost = "http://10.4.41.146:3000/users"; //api link
+        StringRequest request = new StringRequest(Request.Method.POST, urlPost, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println("Post data: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Post data: ERROR");
+            }
+        }) {
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("nombre",  userName.getText().toString());
+                params.put("password",  password.getText().toString());
+                params.put("email", emailAddress.getText().toString());
+                return params;
+
+            }
+            /*
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return  params;
+            }
+
+             */
+
+    //};
+        //requestQueue.add(request);
+
+
+        /*
+        //JSON data in  string format
+        String data = "{"+
+                "\"nombre\"" + "\"" + userName.getText().toString() + "\"," +
+                "\"password\"" + "\"" + password.getText().toString() + "\"," +
+                "\"email\"" + "\"" + emailAddress.getText().toString() + "\"" +
+                "}";
+
+
+        StringRequest request = new StringRequest(Request.Method.POST, urlPost, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //get response on JSONObject
+                try {
+                    JSONObject objRespense = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), objRespense.toString(), Toast.LENGTH_LONG).show(); //print response
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Server Error!", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage() + " ErrorListener", Toast.LENGTH_SHORT).show();
+            }
+        })  {
+            //post data to server
+
+
+        };
+
+        requestQueue.add(request);
+         */
+
+
+    //}
+    //*/
 
 
 }
