4e02e5d9d81692aaf9f125f7e59c323aa3e38994
Adrià Ventura <adria.venturaherce@gmail.com>
2020-03-23 17:34:09 +0100

    Logos de diferentes colores añadidos

--- app/src/main/java/com/pes/fibness/MainActivity.java
+++ app/src/main/java/com/pes/fibness/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -15,26 +16,35 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText userName;
     private EditText password;
     private Button login;
+    private LoginButton loginFb;
     private TextView forgotPassword;
-    private TextView fbLogin;
     private TextView newAccount;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        callbackManager = CallbackManager.Factory.create();
+        loginFb = (LoginButton)findViewById(R.id.login_button);
         userName = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
         login = (Button) findViewById(R.id.btn_login);
         forgotPassword = (TextView) findViewById(R.id.forgot_password);
-        fbLogin = (TextView) findViewById(R.id.fb_login);
         newAccount = (TextView) findViewById(R.id.new_account);
 
         //press to register a new account
@@ -67,8 +77,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loginFb.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                goHomeActivity();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), R.string.cancel_login, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(), R.string.error_login, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 
+    private void goHomeActivity() {
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
