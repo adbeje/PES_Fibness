cbb48469a18d538c3c017ac8da322f8a50b29ac3
tanvir08 <tanvho@instarradell.cat>
2020-04-18 03:47:33 +0200

    ForgotPassword funciona, solo falta la conexion a la bd

--- app/src/main/java/com/pes/fibness/SettingsActivity.java
+++ app/src/main/java/com/pes/fibness/SettingsActivity.java
@@ -1,13 +1,5 @@
 package com.pes.fibness;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.DialogInterface;
@@ -19,12 +11,13 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
-import static android.view.View.*;
+import static android.view.View.OnClickListener;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -38,7 +31,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //loadLocale();
+        loadLocale();
         setContentView(R.layout.activity_settings);
 
 
