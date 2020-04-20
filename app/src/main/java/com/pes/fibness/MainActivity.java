ce5050be1d770206f5eb348ada313a0dbbaa4c81
tanvir08 <tanvir08>
2020-04-21 01:18:53 +0200

    Ver perfil, con estadistica (uso de libreria)

--- app/src/main/java/com/pes/fibness/MainActivity.java
+++ app/src/main/java/com/pes/fibness/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                     emailAddress.setError("Enter a valid email.");
                 else checkUser();
 
-                //homeActivity();
+                ///homeActivity();
             }
         });
 
