4431735d2db6da43efff76a3500ce2c9bdd0b3d6
Adrià Ventura <adria.venturaherce@gmail.com>
2020-05-01 19:42:08 +0200

    List route

--- app/src/main/java/com/pes/fibness/HomeActivity.java
+++ app/src/main/java/com/pes/fibness/HomeActivity.java
@@ -1,13 +1,13 @@
 package com.pes.fibness;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
--- app/src/main/java/com/pes/fibness/RutasFragment.java
+++ app/src/main/java/com/pes/fibness/RutasFragment.java
@@ -1,21 +1,31 @@
 package com.pes.fibness;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
-
-import com.pes.fibness.R;
 
 public class RutasFragment extends Fragment {
 
+    private Button btn;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_rutas, container, false);
+        btn = root.findViewById(R.id.textView5);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), LiteListDemoActivity.class);
+                startActivity(i);
+            }
+        });
+
         return root;
     }
+
 }
