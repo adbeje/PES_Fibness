b9eb65f33ab38121ee6b5442bbb025793d293808
Pascual10 <sergiopg10@hotmail.es>
2020-05-23 21:29:39 +0200

    Refactor Adapters

--- app/src/main/java/com/pes/fibness/CreateTrainingActivity.java
+++ app/src/main/java/com/pes/fibness/CreateTrainingActivity.java
@@ -1,13 +1,19 @@
 package com.pes.fibness;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -360,6 +366,107 @@ public class CreateTrainingActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         isNew = extras.getBoolean("new");
         titleTraining = extras.getString("title");
+    }
+
+}
+
+
+class Exercise_Adap extends BaseAdapter {
+
+    private static LayoutInflater inflater = null;
+
+    private Context context;
+    private ArrayList<Exercise> dades;
+    private boolean New;
+
+    public Exercise_Adap(Context c, ArrayList<Exercise> d, boolean IsNew){
+        context = c;
+        dades = d;
+        New = IsNew;
+        inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return dades.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final View vista = inflater.inflate(R.layout.exerciselist_element, null);
+        if(!New) {
+            TextView titulo = (TextView) vista.findViewById(R.id.ExerciseTitle);
+            TextView repet = (TextView) vista.findViewById(R.id.txtRepet);
+            TextView rest = (TextView) vista.findViewById(R.id.txtRest);
+            TextView series = (TextView) vista.findViewById(R.id.txtSeries);
+            ImageView image = (ImageView) vista.findViewById(R.id.photo_ex);
+            TextView Desc = (TextView) vista.findViewById(R.id.txtDesc);
+            titulo.setText(dades.get(position).TitleEx);
+            int id = getImageID (dades.get(position).TitleEx);
+            if(id != -1) image.setImageResource(id);
+            else{
+                image.setImageResource(R.drawable.logoazulflojo);
+                Desc.setText(dades.get(position).Desc);
+            }
+            series.setText("SERIES: " + dades.get(position).NumSerie);
+            rest.setText("REST: " + dades.get(position).NumRest + "s");
+            repet.setText("REPETITIONS: " + dades.get(position).NumRepet);
+        }
+
+        return vista;
+    }
+
+    private int getImageID(String name){
+        int id = -1;
+        switch (name){
+            case "Squat":
+                id = R.drawable.squat;
+                break;
+            case "Hollow Hold":
+                id = R.drawable.hollow_hold;
+                break;
+            case "Lunges":
+                id = R.drawable.lunges;
+                break;
+            case "Back Extensión Hold":
+                id = R.drawable.back_extension_hold;
+                break;
+            case "Plank":
+                id = R.drawable.plank;
+                break;
+            case "Sit ups":
+                id = R.drawable.sit_ups;
+                break;
+            case "Jumping Jacks":
+                id = R.drawable.jumping_jacks;
+                break;
+            case "Push Up":
+                id = R.drawable.push_up;
+                break;
+            case "Dips":
+                id = R.drawable.dips;
+                break;
+            case "Burpees":
+                id = R.drawable.burpees;
+                break;
+            case "Leg Raises":
+                id = R.drawable.ab_infer;
+                break;
+            case "Bicycle Crunches":
+                id = R.drawable.bicycle_crunches;
+                break;
+        }
+        return id;
     }
 
 }
--- app/src/main/java/com/pes/fibness/FollowingActivity.java
+++ app/src/main/java/com/pes/fibness/FollowingActivity.java
@@ -1,5 +1,6 @@
 package com.pes.fibness;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -7,14 +8,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Pair;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -103,16 +113,149 @@ public class FollowingActivity extends AppCompatActivity implements FollowingAda
         return true;
     }
 
-
-
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(FollowingActivity.this, SearchUsersActivity.class);
         startActivity(intent);
+    }
 
+}
+
+
+class FollowingAdapter extends RecyclerView.Adapter<FollowingAdapter.FollowingAdapterVh> implements Filterable {
+
+    private List<UserModel> userModelList;
+    private Context context;
+    private FollowingAdapter.SelectedUser selectedUser;
+    private List<UserModel> userModelListFiltered;
+    private Boolean first = false;
+
+    public FollowingAdapter(List<UserModel> userModelList, FollowingAdapter.SelectedUser selectedUser) {
+        this.userModelList = userModelList;
+        this.selectedUser = selectedUser;
+        this.userModelListFiltered = userModelList;
+    }
+
+
+    @NonNull
+    @Override
+    public FollowingAdapter.FollowingAdapterVh onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        return new FollowingAdapter.FollowingAdapterVh(LayoutInflater.from(context).inflate(R.layout.row_following, null));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FollowingAdapter.FollowingAdapterVh holder, int position) {
+        UserModel userModel = userModelList.get(position);
+        String username = userModel.getUsername();
+        String prefix = userModel.getUsername().substring(0,1);
+
+        holder.tvUsername.setText(username);
+        holder.tvPrefix.setText(prefix);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userModelList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults filterResults = new FilterResults();
+                if(charSequence == null | charSequence.length() == 0){
+                    filterResults.count = userModelListFiltered.size();
+                    filterResults.values = userModelListFiltered;
+                }
+                else{
+                    String searchChar = charSequence.toString().toLowerCase();
+                    List<UserModel> resultData = new ArrayList<>();
+
+                    for(UserModel userModel: userModelListFiltered){
+                        if(userModel.getUsername().toLowerCase().contains(searchChar)){
+                            resultData.add(userModel);
+                        }
+                    }
+
+                    filterResults.count = resultData.size();
+                    filterResults.values = resultData;
+
+
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                userModelList = (List<UserModel>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+        return filter;
     }
 
 
 
+    public interface SelectedUser{
+        public void selectedUser(UserModel userModel);
+    }
 
+
+
+    public class FollowingAdapterVh extends RecyclerView.ViewHolder {
+        TextView tvPrefix;
+        TextView tvUsername;
+        Button btn_following;
+        public FollowingAdapterVh(@NonNull View itemView) {
+            super(itemView);
+            tvPrefix = itemView.findViewById(R.id.prefix);
+            tvUsername = itemView.findViewById(R.id.username);
+            btn_following = itemView.findViewById(R.id.btn_following);
+
+            tvPrefix.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectedUser.selectedUser(userModelList.get(getAdapterPosition()));
+                }
+            });
+            tvUsername.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectedUser.selectedUser(userModelList.get(getAdapterPosition()));
+                }
+            });
+
+            btn_following.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("my color: " + view.getBackgroundTintList());
+                    if(view.getBackgroundTintList() == null || first){
+                        first = false;
+                        System.out.println("elimiar id: " + userModelList.get(getAdapterPosition()).getId());
+                        ConnetionAPI connetionAPI = new ConnetionAPI(context, "http://10.4.41.146:3001/user/follow/" + User.getInstance().getId() + "/" + userModelList.get(getAdapterPosition()).getId());
+                        connetionAPI.deleteFollowing();
+
+                        view.setBackgroundTintList(ColorStateList.valueOf(-16021062));
+                        btn_following.setText(context.getResources().getText(R.string.Follow));
+                        btn_following.setTextColor(context.getResources().getColor(R.color.white));
+                        System.out.println("my color2222: " + view.getBackgroundTintList());
+                    }
+                    else{
+                        ConnetionAPI connetionAPI = new ConnetionAPI(context, "http://10.4.41.146:3001/user/follow");
+                        connetionAPI.followUser(User.getInstance().getId(), userModelList.get(getAdapterPosition()).getId());
+
+                        view.setBackgroundTintList(ColorStateList.valueOf(0));
+                        btn_following.setText(context.getResources().getText(R.string.Following));
+                        btn_following.setTextColor(context.getResources().getColor(R.color.app_bkg_color));
+                        first = true;
+                    }
+                }
+            });
+
+
+        }
+    }
 }
